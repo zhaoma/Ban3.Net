fb6df064ad522896fc19edad1c70d1f59d776ae8
zhaoma <zhaoma@gmail.com>
2023-04-17 22:18:31 +0800

    define functions to azure instance

---
+++ Platforms/TeamFoundationCollector/Domain/Contract/Interfaces/Functions/IChangesets.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Ban3.Platforms.TeamFoundationCollector.Domain.Contract.Interfaces.Functions
+{
+	public interface IChangeset
+	{
+
+	}
+}
+
---
+++ Platforms/TeamFoundationCollector/Domain/Contract/Interfaces/Functions/ICore.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Ban3.Platforms.TeamFoundationCollector.Domain.Contract.Interfaces.Functions
+{
+	public interface ICore
+	{
+	}
+}
+
