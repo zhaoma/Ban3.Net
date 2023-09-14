9eb6b9f84d2cff1b5598a6dbcd41ec937b36f142
zhifeng.zhao.ext@siemens-healthineers.com <zhaoma@gmail.com>
2023-09-14 17:38:48 +0800

    Update Helper.Mono.cs

--- Infrastructures/Common/Extensions/Helper.Mono.cs
+++ Infrastructures/Common/Extensions/Helper.Mono.cs
@@ -1,4 +1,8 @@
-﻿namespace Ban3.Infrastructures.Common.Extensions;
+﻿/* -------------------------------------------------------------------------------------------------
+   Copyright (C) Siemens Healthcare GmbH 2023, All rights reserved. Restricted.
+   ------------------------------------------------------------------------------------------------- */
+   
+namespace Ban3.Infrastructures.Common.Extensions;
 
 public static partial class Helper
 {
