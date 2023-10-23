d95be68eaa1300afd8e454af59aa9fc79ad325e5
zhaoma <zhaoma@gmail.com>
2023-10-23 21:02:03 +0800

    Update ICalendarsSynchronizer.cs

--- Infrastructures/ServiceCentre/Applications/Kalendar/ICalendarsSynchronizer.cs
+++ Infrastructures/ServiceCentre/Applications/Kalendar/ICalendarsSynchronizer.cs
@@ -1,6 +1,63 @@
-﻿namespace Ban3.Infrastructures.ServiceCentre.Applications.Kalendar;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ban3.Infrastructures.ServiceCentre.Entries.Kalendar;
+
+namespace Ban3.Infrastructures.ServiceCentre.Applications.Kalendar;
 
 public interface ICalendarsSynchronizer
 {
-    
+
+    Task<bool> TrySaveGroup(
+ICalendarPlatform calendarPlatform,
+ICalendarOwner calendarOwner,
+ICalendarGroup calendarGroup
+);
+
+    Task<bool> TryRetrieveGroups(
+    ICalendarPlatform calendarPlatform,
+    ICalendarOwner calendarOwner,
+    out IEnumerable<ICalendarGroup> calendarGroups);
+
+    Task<bool> TryDeleteGroup(
+ICalendarPlatform calendarPlatform,
+ICalendarOwner calendarOwner,
+ICalendarGroup calendarGroup
+    );
+
+    Task<bool> TrySaveCalendar(
+    ICalendarPlatform calendarPlatform,
+    ICalendarOwner calendarOwner,
+    ICalendar calendar
+    );
+
+    Task<bool> TryRetrieveCalendars(
+    ICalendarPlatform calendarPlatform,
+    ICalendarOwner calendarOwner,
+    out IEnumerable<ICalendar> calendars);
+
+    Task<bool> TryDeleteCalendar(
+ICalendarPlatform calendarPlatform,
+ICalendarOwner calendarOwner,
+ICalendar calendar
+    );
+
+
+    Task<bool> TrySaveEvent(
+ICalendarPlatform calendarPlatform,
+ICalendarOwner calendarOwner,
+ICalendarEvent calendarEvent
+);
+
+    Task<bool> TryRetrieveEvents(
+    ICalendarPlatform calendarPlatform,
+    ICalendarOwner calendarOwner,
+    out IEnumerable<ICalendarEvent> calendarEvents);
+
+    Task<bool> TryDeleteEvent(
+ICalendarPlatform calendarPlatform,
+ICalendarOwner calendarOwner,
+ICalendarEvent calendarEvent
+    );
+
+
 }
