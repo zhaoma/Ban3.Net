b61230b398ee91d793ec8c9d112997d48bf04ddc
zhaoma <zhaoma@gmail.com>
2023-10-25 22:54:44 +0800

    sen

---
+++ Infrastructures/WebAutomator/Browser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Ban3.Infrastructures.WebAutomator;
+
+public class OneBrowser :IDisposable
+{
+    private IWebDriver webDriver;
+
+    public OneBrowser()
+    {
+        webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+    }
+
+
+
+    public void Dispose()
+    {
+        if (webDriver != null)
+        {
+            webDriver.Close();
+            webDriver.Dispose();
+            webDriver.Quit();
+        }
+    }
+}
+
